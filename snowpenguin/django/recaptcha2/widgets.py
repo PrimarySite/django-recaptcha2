85b3273be22a89e8cc1d17a2f13acac472fe8cbc
Marcin Rutkowski <marcin@primarysite.net>
2017-03-17 16:34:01 +0000

    select element by class name

--- snowpenguin/django/recaptcha2/widgets.py
+++ snowpenguin/django/recaptcha2/widgets.py
@@ -1,3 +1,4 @@
+import uuid
 from django.conf import settings
 from django.forms.widgets import Widget
 from django.template.loader import render_to_string
@@ -24,6 +25,7 @@ class ReCaptchaWidget(Widget):
         return mark_safe(
             render_to_string(template, {
                 'container_id': 'id_%s' % name,
+                'container_uid': 'captcha_{0}'.format(uuid.uuid4().hex),
                 'public_key': settings.RECAPTCHA_PUBLIC_KEY,
                 'theme': self.theme,
                 'type': self.type,
